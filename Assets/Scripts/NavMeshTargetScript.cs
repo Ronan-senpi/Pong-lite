d6fc363bd66a6010cc1cb3c97c522fee74cc263e
Ronan <30617930+Ronan-senpai@users.noreply.github.com>
2020-09-29 09:40:15 +0200

    Animation - Mesh agant

---
+++ Assets/Scripts/NavMeshTargetScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshTargetScript : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject target;
+    [SerializeField]
+    private NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    private void Update()
+    {
+        agent.SetDestination(target.transform.position);
+        if (agent.isStopped && agent.pathStatus == NavMeshPathStatus.PathComplete)
+        {
+            StartCoroutine(BlowUp());
+        }
+    }
+
+    IEnumerator BlowUp()
+    {
+        yield return new WaitForSeconds(1.5f);
+        Debug.LogError("BOOOOM !");
+        Destroy(gameObject);
+    }
+
+}
