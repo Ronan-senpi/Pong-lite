da25255fe5db2723c6fe5041f58ce4cebe109959
Ronan <30617930+Ronan-senpai@users.noreply.github.com>
2020-09-29 08:49:53 +0200

    Ui

---
+++ Assets/Scripts/BtnScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BtnScript : MonoBehaviour
+{
+    public void click()
+    {
+        Debug.Log("Click !");
+    }
+}
